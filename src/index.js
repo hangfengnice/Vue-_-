97fce7255398f5e2eab26921183b20a09512e843
李莹莹 <liyingying@liyingyingdeMacBook-Pro.local>
2019-05-23 20:57:43 +0800

    加了轮播图 首页的样式 各个页面之间的切换

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
+import {Swipe,SwipeItem} from 'mint-ui'
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem)
 Vue.use(MintUI)
 
 import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
 
+import router from './router.js'
 import app from './App.vue'
 var vm = new Vue({
     el:'#app',
-    render:c=>c(app)
+    render:c=>c(app),
+    router
 })
